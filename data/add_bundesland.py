fedcb9c2e51cd5ab7dbd206ef11a83fff7850038
Tim Reichelt <treichelt6@gmail.com>
2016-08-22 15:35:07 +0200

    add providers-chart and redraw map on selection

--- data/add_bundesland.py
+++ data/add_bundesland.py
@@ -2,13 +2,46 @@ import json
 from shapely.geometry import Point, shape
 from tqdm import tqdm
 
+
 def get_bundesland(longitude, latitude, bundeslaender):
     point = Point(longitude, latitude)
+
     for bundesland in bundeslaender["features"]:
         polygon = shape(bundesland["geometry"])
         if polygon.contains(point):
             return bundesland["properties"]["NAME_1"]
+
     return "other"
+
+
+def make_features(feature):
+    providers = ["all", "t-mobile", "vodafone", "e-plus", "o2"]
+    features = []
+
+    try:
+        first_long = feature["geometry"]["coordinates"][0][0] 
+        first_lat = feature["geometry"]["coordinates"][0][1]
+        bundesland = get_bundesland(first_long, first_lat, bundeslaender)
+    except:
+        # exit if we cannot determine bundesland
+        return
+
+    for provider in providers:
+        stability = feature["properties"][provider + "_stability"]
+        measurements = feature["properties"][provider + "_measurements"]
+
+        if measurements == 0:
+            continue
+
+        features.append({
+            "type": feature["type"],
+            "geometry": feature["geometry"],
+            "properties": {"stability": stability, "measurements": measurements},
+            "provider": provider,
+            "bundesland": bundesland
+        })
+
+    return features
 
 with open("connectivity.geojson") as f:
     data = json.load(f)
@@ -16,16 +49,18 @@ with open("connectivity.geojson") as f:
 with open("bundeslaender.geojson") as f:
     bundeslaender = json.load(f)
 
+featureCollection = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
 for feature in tqdm(data["features"]):
-    try:
-        first_long, first_lat = feature["geometry"]["coordinates"][0][0], feature["geometry"]["coordinates"][0][1]
-        feature["bundesland"] = get_bundesland(first_long, first_lat, bundeslaender)
-    except:
-        feature["bundesland"] = "other"
+    newFeatures = make_features(feature)
+    featureCollection["features"].extend(newFeatures)
 
 
 with open("bundeslaender_connectivity.geojson", "w+") as f:
-    json.dump(data, f)
+    json.dump(featureCollection, f)
 
 # TODO: Transform decimals into percent values
 # TODO: Clean data
--- static/js/graphs.js
+++ static/js/graphs.js
@@ -9,27 +9,25 @@ function makeGraphs(error, geojson) {
     }
 
     var records = geojson.features;
-    records.forEach(function(d) {
-        d.properties.all_stability = parseFloat(d.properties.all_stability);
-        d.geometry.coordinates.forEach(function(coords) {
-            coords[0] = +coords[0];
-            coords[1] = +coords[1];
-        });
-    });
 
     // Create Crossfilter instance
     var ndx = crossfilter(records);
 
     // Define dimensions
-    var bundeslandDim = ndx.dimension(function(d) { return d.bundesland ? d.bundesland : ""; });
+    var bundeslandDim = ndx.dimension(function(d) { return d.bundesland; });
+    var providerDim = ndx.dimension(function(d) { return d.provider; });
+    var allDim = ndx.dimension(function(d) { return d; });
 
     // Group data
-    var stability = "all_stability";
     var bundeslandGroup = bundeslandDim.group()
-    .reduce(reduceAddAvg(stability), reduceRemoveAvg(stability), reduceInitAvg);
+                                       .reduce(reduceAddAvg, reduceRemoveAvg, reduceInitAvg);
+
+    var providerGroup = providerDim.group()
+                                       .reduce(reduceAddAvg, reduceRemoveAvg, reduceInitAvg);
 
     // Create charts
     var bundeslandChart = dc.rowChart("#bundesland-row-chart");
+    var providerChart = dc.rowChart("#provider-row-chart");
 
     bundeslandChart
     .width(400)
@@ -40,44 +38,59 @@ function makeGraphs(error, geojson) {
     .valueAccessor(function(d) { return d.value.avg; })
     .colors(['#6baed6'])
     .labelOffsetY(12)
-    .xAxis().tickValues([0, 0.5, 1]);
+    .xAxis().tickValues([0, 0.5, 1]).ticks(3);
 
-    var map = L.map("map");
+    providerChart
+    .width(400)
+    .height(420)
+    .dimension(providerDim)
+    .group(providerGroup)
+    .ordering(function(d) { return -d.value.avg; })
+    .valueAccessor(function(d) { return d.value.avg; })
+    .colors(['#6baed6'])
+    .labelOffsetY(12)
+    .xAxis().tickValues([0, 0.5, 1]).ticks(3);
+
+    L.mapbox.accessToken = "pk.eyJ1IjoidHJlaWdlcm0iLCJhIjoiY2lzNXU4bzllMDAwZTJ5bXcwajA1ZjdvYSJ9.OA1zmwAiQpIqL3tcHmBddg";
+    var map = L.mapbox.map("map", "mapbox.streets");
+    var connectivityLayer = L.mapbox.featureLayer();
 
     var drawMap = function() {
         map.setView([51.9, 10.26], 5);
-        mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 15,
-        }).addTo(map);
 
-        L.geoJson(records).addTo(map);
+        connectivityLayer.setGeoJSON(allDim.top(Infinity)).addTo(map);
     };
 
+    providerChart.filter("all");
+
     drawMap();
+
+    dcCharts = [bundeslandChart, providerChart];
+
+    for (i = 0; i < dcCharts.length; i++) {
+        dcCharts[i].on("filtered", function (chart, filter) {
+            drawMap();
+        });
+    }
 
     // Display the dc graphs
     dc.renderAll();
 }
 
-function reduceAddAvg(attr) {
-  return function(p,v) {
+function reduceAddAvg(p, v) {
     ++p.count;
-    p.sum += v.properties[attr];
+    p.sum += v.properties.stability;
     p.avg = p.sum/p.count;
     return p;
-  };
 }
-function reduceRemoveAvg(attr) {
-  return function(p,v) {
+
+function reduceRemoveAvg(p, v) {
     --p.count;
-    p.sum -= v.properties[attr];
+    p.sum -= v.properties.stability;
     p.avg = p.sum/p.count;
     return p;
-  };
 }
+
 function reduceInitAvg() {
   return {count:0, sum:0, avg:0};
 }
